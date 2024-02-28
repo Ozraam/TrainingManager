badcb58936d1e3ed5ac7d2e78412caa9aaa3c600
ARDOUIN Tristan <tristan.ardouin3@etu.univ-lorraine.fr>
2024-02-28 16:36:10 +0200

    ✨ added competence not acquired by operator

--- components/operator/OperatorDetails.vue
+++ components/operator/OperatorDetails.vue
@@ -10,6 +10,10 @@ const sp = useSupabaseClient()
 
 const { data } = await sp.from('Person').select('*, Role (*), Validity(*, Competence (*))').eq('id_pers', props.currentOperator)
 
+const { data: data2 } = await sp.from('Competence').select('*')
+
+const dataCompetence : { id_comp: number, name: string, id_role: number }[] | null = data2
+
 const operator : Ref<{
     id_pers: number,
     name: string,
@@ -30,6 +34,11 @@ const operator : Ref<{
         }
     }[]
 } | null> = ref(data?.[0] ?? null)
+
+// filter out competences that are operator doesn't have, operator can have competences from different roles
+const competenceFilter : { id_comp: number, name: string, id_role: number }[] = computed(() => {
+    return dataCompetence?.filter(competecence => !operator.value?.Validity.some(validity => validity.Competence.id_comp === competecence.id_comp))
+})
 </script>
 
 <template>
@@ -63,6 +72,17 @@ const operator : Ref<{
                     </span>
 
                     <span v-if="!competence.verified"> : {{ $t("operator.renew") }}</span>
+                </li>
+
+                <li
+                    v-for="competence in competenceFilter"
+                    :key="competence.id_comp"
+                >
+                    {{ competence.name }} : <span
+                        class="text-red-500"
+                    >
+                        {{ $t("operator.not_verified") }}
+                    </span>
                 </li>
             </ul>
         </div>
