90d346b201bb49fa5d95241d14faf765e5163d85
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2024-03-08 17:13:53 +0200

    change in the file (add a new fontionnality)

--- server/api/OperatorsInfos.get.ts
+++ server/api/OperatorsInfos.get.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable indent */
 import { serverSupabaseClient } from '#supabase/server'
 export default defineEventHandler(async (event) => {
@@ -5,6 +6,30 @@ export default defineEventHandler(async (event) => {
 
     const { data } = await sp.from('Operators')
     .select('id_op, name, surname, Position(*, Position_comp(*, Competences(*))),  Registration(*, Training(*), State(name))')
+
+    data?.forEach((_operator: any) => {
+        _operator?.Position?.Position_comp?.forEach((_comp: any) => {
+            _operator?.Registration?.forEach((_reg: any) => {
+                if (_reg?.Training?.id_comp === _comp?.id_comp) {
+                    console.log(_reg?.Training?.date)
+                    const now = new Date()
+                    const trainingDate = new Date(_reg?.Training?.date)
+                    const validityDays = _comp?.Competences?.tmp_validity || 0
+
+                    // Ajouter le nombre de jours de validité à la date de formation
+                    trainingDate.setDate(trainingDate.getDate() + validityDays)
+
+                    if (now.getDate() > trainingDate.getDate()) {
+                        _comp.Competence.valid = false
+                    } else {
+                        _comp.Competence.valid = true
+                    }
+                } else {
+                    console.log(_comp)
+                }
+            })
+        })
+    })
 
     return {
         data
