8a74a69265c4328b20d781496dca20dcbf7a5957
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2024-03-08 18:25:18 +0200

    the fonctionality work pb of name

--- server/api/OperatorsInfos.get.ts
+++ server/api/OperatorsInfos.get.ts
@@ -1,31 +1,31 @@
 /* eslint-disable no-console */
 /* eslint-disable indent */
 import { serverSupabaseClient } from '#supabase/server'
+import { Operator, PositionComp, Registration } from '~/utils/types'
 export default defineEventHandler(async (event) => {
     const sp = await serverSupabaseClient(event)
 
     const { data } = await sp.from('Operators')
-    .select('id_op, name, surname, Position(*, Position_comp(*, Competences(*))),  Registration(*, Training(*), State(name))')
+    .select('*, Position(*, Position_comp(*, Competences(*))),  Registration(*, Training(*), State(name))')
 
-    data?.forEach((_operator: any) => {
-        _operator?.Position?.Position_comp?.forEach((_comp: any) => {
-            _operator?.Registration?.forEach((_reg: any) => {
-                if (_reg?.Training?.id_comp === _comp?.id_comp) {
-                    console.log(_reg?.Training?.date)
+    data?.forEach((operator: Operator) => {
+        operator.Position.Position_comp.forEach((comp: PositionComp) => {
+            operator.Registration.forEach((reg: Registration) => {
+                if (reg.Training.id_comp === comp.id_comp) {
+                    console.log(reg.Training.date)
                     const now = new Date()
-                    const trainingDate = new Date(_reg?.Training?.date)
-                    const validityDays = _comp?.Competences?.tmp_validity || 0
+                    const trainingDate = new Date(reg.Training.date)
+                    const validityDays = comp.Competences.tmp_validity || 0
 
-                    // Ajouter le nombre de jours de validité à la date de formation
                     trainingDate.setDate(trainingDate.getDate() + validityDays)
 
                     if (now.getDate() > trainingDate.getDate()) {
-                        _comp.Competence.valid = false
+                        comp.Competences.valid = false
                     } else {
-                        _comp.Competence.valid = true
+                        comp.Competences.valid = true
                     }
                 } else {
-                    console.log(_comp)
+                    comp.Competences.valid = false
                 }
             })
         })
