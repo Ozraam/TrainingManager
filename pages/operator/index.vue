020c3990231ba83b2aaefd838746d0ba6cf30f28
ARDOUIN Tristan <tristan.ardouin3@etu.univ-lorraine.fr>
2024-03-12 10:51:21 +0200

    ✨ possible to delete operators

---
+++ pages/operator/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const sp = useSupabaseClient()
+
+const { data } = await sp.from('Operators').select('id_op').order('name')
+
+navigateTo(`/operator/${data![0].id_op}`)
+</script>
