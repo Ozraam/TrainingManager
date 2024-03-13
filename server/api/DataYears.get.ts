235e003d3c00a1f9a895a3cac42b0fc83602c199
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2024-03-13 16:24:50 +0200

    add of a new version of datayear

--- server/api/DataYears.get.ts
+++ server/api/DataYears.get.ts
@@ -1,15 +1,55 @@
+/* eslint-disable no-console */
 import { serverSupabaseClient } from '#supabase/server'
-import { Competence, YearData } from '~/utils/types'
+import { Competence, Registration, Training, YearData } from '~/utils/types'
 export default defineEventHandler(async (event) => {
     const sp = await serverSupabaseClient(event)
+    const data = [] as any
 
     const dataFocast = (await sp.from('Forecast').select('*')).data as YearData[]
-    const dataCompetences = (await sp.from('Competences').select('*, Training(*, Registration(*, Operators(*)))')).data as Competence[]
+    const dataCompetences = (await sp.from('Competences').select('*, Training(*, Registration(*, Operators(*), State(*)))')).data as Competence[]
+    data.push(dataCompetences)
+    // console.log(dataFocast)
+    // console.log(dataCompetences)
 
-    const data = {
-        dataFocast,
-        dataCompetences
-    }
+    dataFocast.forEach((yearData) => {
+        data.push({
+            year: yearData.year,
+            budget: yearData.budget,
+            competences: []
+        })
+    })
+
+    dataCompetences.forEach((competence) => {
+        data.forEach((yearData: YearData) => {
+            const Competence = {} as Competence
+            competence.Training.forEach((training:Training) => {
+                if (yearData.year === new Date(training.date).getFullYear()) {
+                    Competence.name = competence.name
+                    Competence.Training = []
+                    if (training.id_comp === competence.id_comp) {
+                        const Training = {} as Training
+                        Training.name = training.name
+                        Training.date = training.date
+                        Training.duration = training.duration
+                        Training.cost = training.cost
+                        Training.operators = []
+
+                        training.Registration.forEach((registration:Registration) => {
+                            if (registration.Operators.length !== 0 && training.id_train === registration.id_train) {
+                                console.log(registration.Operators)
+                                Training.operators = registration.Operators
+                                Training.state = registration.State.name
+                            }
+                        })
+
+                        Competence.Training.push(Training)
+                    }
+                    yearData.competences.push(Competence)
+                }
+            })
+        })
+    })
+
     return {
         data
     }
