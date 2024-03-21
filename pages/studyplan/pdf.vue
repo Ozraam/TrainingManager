1e85c6ec2a5286263dea9b5d62e1e84a1fda4ed8
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2024-03-21 16:28:09 +0200

    better UI for the Downloaded

--- pages/studyplan/pdf.vue
+++ pages/studyplan/pdf.vue
@@ -4,7 +4,7 @@ import { onMounted, ref } from 'vue'
 import type { StudyPlan } from '../../utils/types'
 const tmp = ref<{ data: StudyPlan[] }>()
 const page = ref<HTMLElement>()
-
+const comp = ref<StudyPlan[][]>([])
 async function fetchData() {
     try {
         tmp.value = await $fetch('/api/StudyPlanHelper', {
@@ -16,7 +16,17 @@ async function fetchData() {
         })
 
         if (tmp.value && tmp.value.data) {
-            // console.log(tmp.value.data)
+            console.log(tmp.value.data)
+
+            tmp.value.data.forEach((item) => {
+                const i = comp.value.findIndex((el:StudyPlan[]) => el[0].id_comp === item.id_comp)
+                if (i === -1) {
+                    comp.value.push([item])
+                } else {
+                    comp.value[i].push(item)
+                }
+            })
+            console.log(comp.value)
         } else {
             console.error('La réponse du serveur ne contient pas de données valides.')
         }
@@ -25,9 +35,32 @@ async function fetchData() {
     }
 }
 
+function replaceAllLatvianLongVowelsInHtmlElement(element: HTMLElement) {
+    // replace all long vowels with short ones in the text of the element children check if the element is a text node
+    if (element.nodeType === Node.TEXT_NODE) {
+        element.textContent = element.textContent
+            .replace(/ā/g, 'aa')
+            .replace(/ē/g, 'ee')
+            .replace(/ī/g, 'ii')
+            .replace(/ū/g, 'uu')
+            .replace(/Ā/g, 'AA')
+            .replace(/Ē/g, 'EE')
+            .replace(/Ī/g, 'II')
+            .replace(/Ū/g, 'UU')
+            .replace(/ģ/g, 'g')
+            .replace(/ķ/g, 'k')
+            .replace(/ļ/g, 'l')
+            .replace(/ņ/g, 'n')
+    } else {
+        // if the element is not a text node, then we need to check its children
+        element.childNodes.forEach(replaceAllLatvianLongVowelsInHtmlElement)
+    }
+}
+
 function DownloadPDF() {
-    console.log(page.value)
-    exportToPDF('test.pdf', page.value)
+    // console.log(page.value)
+    replaceAllLatvianLongVowelsInHtmlElement(page.value!)
+    exportToPDF('Study-plan.pdf', page.value!, { format: 'a4' }, { html2canvas: { scale: 0.7 } })
 }
 
 onMounted(async () => {
@@ -37,7 +70,7 @@ onMounted(async () => {
 
 <template>
     <div
-        class="flex justify-center gap-5"
+        class="flex justify-center gap-5 p-8"
     >
         <div
             ref="page"
@@ -48,20 +81,37 @@ onMounted(async () => {
             </h1>
 
             <div
-                v-for="item in tmp?.data"
-                :key="item.id_op"
-                class="flex gap-5 "
+                v-for="items in comp"
+                :key="items[0].id_comp"
+                class="flex justify-center gap-5 mb-5"
             >
                 <div
-                    class="flex flex-col gap-5"
+                    class="flex flex-col gap-5 border border-black p-5 w-3/4"
                 >
-                    <h2 class="text-xl font-bold">
-                        {{ item.name }}
+                    <h2 class="text-2xl font-bold text-center">
+                        Comptence: {{ items[0].name }}
                     </h2>
 
-                    <p>
-                        {{ item.name_op + ' ' + item.surname }}
-                    </p>
+                    <div
+                        v-for="item in items"
+                        :key="item.id_op"
+                    >
+                        <p class="text-xl">
+                            Name: {{ item.name_op + ' ' + item.surname }}
+                        </p>
+
+                        <p>
+                            Name of the last Training: {{ item.name_train }}
+                        </p>
+
+                        <p>
+                            Date of the last Training: {{ item.date }}
+                        </p>
+
+                        <p>
+                            State: {{ item.name_state }}
+                        </p>
+                    </div>
                 </div>
             </div>
 
