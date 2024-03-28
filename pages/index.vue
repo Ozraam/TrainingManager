e5cf14f198ea702d8340dcc4c68b8b9db0e3d260
Tristan A. <lhgrillepain@gmail.com>
2024-03-28 10:18:48 +0200

    add temp sign in

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+const sp = useSupabaseClient()
+
+sp.auth.signInWithPassword({
+    email: 'lhgrillepain@gmail.com',
+    password: 'salutlafamille'
+})
+</script>
+
 <template>
     <div class="min-h-screen">
         <PageHeader :title="$t('home.title')" />
