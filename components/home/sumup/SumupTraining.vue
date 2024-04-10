8bdeb575e575c9fbc5e16b07e1d621c17d93d876
Tristan A. <lhgrillepain@gmail.com>
2024-04-10 16:21:30 +0300

    ♻️ Refactor training list components to show percent not all people

--- components/home/sumup/SumupTraining.vue
+++ components/home/sumup/SumupTraining.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import RegistrationSumup from './RegistrationSumup.vue'
-defineProps({
+const props = defineProps({
     training: {
         type: Object as PropType<Training>,
         required: true
@@ -8,31 +7,75 @@ defineProps({
 })
 
 defineEmits(['update'])
+
+const numberOfTrainingDone = ref(props.training.Registration.filter(registration => registration.State.name === 'done').length)
+const numberOfTrainingDelayed = ref(props.training.Registration.filter(registration => registration.State.name === 'delayed').length)
 </script>
 
 <template>
-    <UCard>
-        <template #header>
-            <div class="flex justify-between items-center">
-                <h3>
-                    {{ training.name }}
-                </h3>
+    <div>
+        <div class="flex justify-between items-center gap-4">
+            <UButton
+                :to="`/training/${training.id_train}`"
+                variant="link"
+            >
+                {{ training.name }}
+            </UButton>
 
-                <UButton
-                    label="See Training"
-                    :to="`/training/${training.id_train}`"
-                    variant="link"
-                />
+            <div>
+                <span class="text-gray-500 dark:text-gray-400 flex text-center">
+                    <div class="flex flex-col">
+                        <span>
+                            Done
+                        </span>
+
+                        <span>
+                            {{ numberOfTrainingDone }}
+                        </span>
+                    </div>
+
+                    <UDivider
+                        orientation="vertical"
+                        size="md"
+                    />
+
+                    <div class="flex flex-col">
+                        <span>
+                            Delayed
+                        </span>
+
+                        <span>
+                            {{ numberOfTrainingDelayed }}
+                        </span>
+                    </div>
+
+                    <UDivider
+                        orientation="vertical"
+                        size="md"
+                    />
+
+                    <div>
+                        <span>
+                            Total
+                        </span>
+
+                        <span>
+                            {{ training.Registration.length }}
+                        </span>
+                    </div>
+
+                </span>
             </div>
-        </template>
 
-        <div class="flex flex-col gap-2">
-            <RegistrationSumup
-                v-for="registration in training.Registration"
-                :key="registration.id_op"
-                :registration="registration"
-                @update="$emit('update')"
+            <div>
+                Date: {{ new Date(training.date).toLocaleDateString() }}
+            </div>
+
+            <UButton
+                label="See more detail"
+                :to="`/dashboard-detail?training=${training.id_train}`"
+                variant="link"
             />
         </div>
-    </UCard>
+    </div>
 </template>
