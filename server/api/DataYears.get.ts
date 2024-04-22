3bbb8ccf71cd2f8a5bb2919cdceeb83362b2222a
Tristan A. <lhgrillepain@gmail.com>
2024-04-22 10:49:48 +0300

    🍱 change fav icon and order year

--- server/api/DataYears.get.ts
+++ server/api/DataYears.get.ts
@@ -4,11 +4,8 @@ export default defineEventHandler(async (event) => {
     const sp = await serverSupabaseClient(event)
     const data = [] as YearData[]
 
-    const dataFocast = (await sp.from('Forecast').select('*')).data as YearData[]
+    const dataFocast = (await sp.from('Forecast').select('*').order('year')).data as YearData[]
     const dataCompetences = (await sp.from('Competences').select('*, Training(*, Registration(*, Operators(*), State(*)))')).data as Competence[]
-    // data.push(dataCompetences)
-    //
-    //
 
     dataFocast.forEach((yearData) => {
         data.push({
